539f7290df71eb026d7fb297d540adf87f74fa3e
EladTal™ <48048436+eladtpro@users.noreply.github.com>
2022-08-09 07:53:25 +0000

    build image

---
+++ .history/server/server_20220809074916.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const requestIp = require('request-ip');
+
+// Constantsdgdsgsdg
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  const clientIp = requestIp.getClientIp(req);
+  console.log(`New request from CLIENT: ${clientIp}, called on ${new Date().toTimeString()}`);
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);

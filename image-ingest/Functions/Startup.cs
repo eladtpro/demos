aa6ae87ebec04798c37e613f0b2dd09facefc3f2
eladtpro <eladt.pro@gmail.com>
2022-08-30 18:42:42 +0300

    dictionary

---
+++ image-ingest/Functions/Startup.cs
@@ -0,0 +1,42 @@
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+[assembly: FunctionsStartup(typeof(MyApplication.Startup))]
+namespace MyApplication
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IMessageSerializerSettingsFactory, CustomMessageSerializerSettingsFactory>();
+            builder.Services.AddSingleton<IErrorSerializerSettingsFactory, CustomErrorSerializerSettingsFactory>();
+        }
+
+        /// <summary>
+        /// A factory that provides the serialization for all inputs and outputs for activities and
+        /// orchestrations, as well as entity state.
+        /// </summary>
+        internal class CustomMessageSerializerSettingsFactory : IMessageSerializerSettingsFactory
+        {
+            public JsonSerializerSettings CreateJsonSerializerSettings()
+            {
+                // Return your custom JsonSerializerSettings here
+            }
+        }
+
+        /// <summary>
+        /// A factory that provides the serialization for all exceptions thrown by activities
+        /// and orchestrations
+        /// </summary>
+        internal class CustomErrorSerializerSettingsFactory : IErrorSerializerSettingsFactory
+        {
+            public JsonSerializerSettings CreateJsonSerializerSettings()
+            {
+                // Return your custom JsonSerializerSettings here
+            }
+        }
+    }
+}
